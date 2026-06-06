10a5a93706f5653faadf94ca6eb237e2703dfdcf
orderly-guoqing <guoqing@orderly.network>
2026-06-07 00:37:41 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Swap,Vaults,Campaigns",
-  "VITE_CUSTOM_MENUS": "Create DEX, https://dex.orderly.network/dex?distributor_code=PRIMEX",
+  "VITE_CUSTOM_MENUS": "Create DEX, https://dex.orderly.network/dex?distributor_code=PRIMEX; Primex Vault, https://app.orderly.network/vaults/sp/0x37373f70006a742ac6c0042e0b3665966501c060",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
   "VITE_ENABLE_CAMPAIGNS": "true",
   "VITE_TELEGRAM_URL": "",
